1b106ac507c68e0b5c804e84b8ba94dd78a135ad
guoyifan <guoyifan@cmcm.com>
2019-12-09 13:03:20 +0800

    feat: update template

--- config/template.config.js
+++ config/template.config.js
@@ -9,6 +9,6 @@ module.exports = [
         name: 'mobile-react',
         address: 'git@github.com:raiden-group/rd-template-mobile-react.git',
         packageName: 'rd-template-mobile-react',
-        description: '移动端单叶应用模版'
+        description: '移动端单页应用模版'
     }
 ]
