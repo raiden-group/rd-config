8ab5483b4d0eeb5127dcdd04a5070949fd88ca21
guoyifan <guoyifan@cmcm.com>
2019-12-09 11:28:22 +0800

    feat: 配置模板文件

---
+++ config/template.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        name: 'react-mobx',
+        address: 'git@github.com:raiden-group/rd-template-react-mobx.git',
+        packageName: 'rd-template-react-mobx',
+        description: '这是一个react-mobx的模版',
+    },
+    {
+        name: 'mobile-react',
+        address: 'git@github.com:raiden-group/rd-template-mobile-react.git',
+        packageName: 'rd-template-mobile-react',
+        description: '移动端单叶应用模版'
+    }
+]
