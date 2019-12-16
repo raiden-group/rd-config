c6f614018581eb0eb87e091a7b5667ce7ba9d514
yifan <2930742424@qq.com>
2019-12-16 13:14:36 +0800

    Update template.config.js

--- config/template.config.js
+++ config/template.config.js
@@ -10,5 +10,11 @@ module.exports = [
         address: 'git@github.com:raiden-group/rd-template-mobile-react.git',
         packageName: 'rd-template-mobile-react',
         description: '移动端单页应用模版'
+    },
+    {
+        name: 'component',
+        address: 'git@github.com:raiden-group/rd-template-component.git',
+        packageName: 'rd-template-component',
+        description: 'react 组件模版'
     }
 ]
