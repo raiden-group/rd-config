b9e64cbc72e2ce74b0b132cf775aa70a878f6e6c
yifan <2930742424@qq.com>
2020-04-09 14:01:34 +0800

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         name: 'react-mobx',
         address: 'git@github.com:raiden-group/rd-template-react-mobx.git',
+        version: 0.0.2,
         packageName: 'rd-template-react-mobx',
         description: '这是一个react-mobx的模版',
     },
@@ -9,12 +10,14 @@ module.exports = [
         name: 'mobile-react',
         address: 'git@github.com:raiden-group/rd-template-mobile-react.git',
         packageName: 'rd-template-mobile-react',
+        version: 0.0.1,
         description: '移动端单页应用模版'
     },
     {
         name: 'component',
         address: 'git@github.com:raiden-group/rd-template-component.git',
         packageName: 'rd-template-component',
+        version: 0.0.1,
         description: 'react 组件模版'
     }
 ]
